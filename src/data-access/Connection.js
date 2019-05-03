dcaedadcfc8effdd89054efd2c8b8dd21d77b02c
Rhys <Rhys26@hotmail.com>
2019-05-03 23:14:03 +0100

    Renamed data folder to be data-access

---
+++ src/data-access/Connection.js
@@ -0,0 +1,6 @@
+const ConnectionString = 'http://localhost:5000'
+//const ConnectionString = 'http://46.101.47.14:5000'
+
+module.exports = {
+    ConnectionString: ConnectionString
+}
--- src/data/Connection.js
+++
@@ -1,4 +0,0 @@
-//const ConnectionString = 'http://localhost:5000'
-const ConnectionString = 'http://46.101.47.14:5000'
-
-export default ConnectionString
