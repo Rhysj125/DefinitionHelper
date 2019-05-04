7616514931953d752d44068e708d341c9a276842
Rhys <Rhys26@hotmail.com>
2019-05-04 14:16:46 +0100

    Connection String swap out for go live

--- src/data-access/Connection.js
+++ src/data-access/Connection.js
@@ -1,5 +1,5 @@
-const ConnectionString = 'http://localhost:5000'
-//const ConnectionString = 'http://46.101.47.14:5000'
+//const ConnectionString = 'http://localhost:5000'
+const ConnectionString = 'http://46.101.47.14:5000'
 
 module.exports = {
     ConnectionString: ConnectionString
